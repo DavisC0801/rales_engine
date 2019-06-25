57f0a72791c7dc48e92f614b35b16ed4a9eae9fa
davisc0801 <davisc1014@gmail.com>
2019-06-25 11:34:18 -0600

    Convert credit card number to string

--- db/migrate/20190625021246_createtransactions.rb
+++ db/migrate/20190625021246_createtransactions.rb
@@ -2,9 +2,8 @@ class Createtransactions < ActiveRecord::Migration[5.2]
   def up
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
-      t.integer :credit_card_number
-      t.integer :credit_card_experation_date
-      t.integer :quantity
+      t.string :credit_card_number
+      t.integer :credit_card_expiration_date
       t.string :result
       t.timestamps
     end
