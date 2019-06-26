c5787ab0fc0a0bd9cdfbc5e34ee1321ce72f2125
davisc0801 <davisc1014@gmail.com>
2019-06-26 11:49:47 -0600

    Add item controller

---
+++ app/controllers/items/items_records_controller.rb
@@ -0,0 +1,34 @@
+class ItemsRecordsController < ApplicationController
+  acceptable_queries = ["id", "name", "description", "unit_price", "merchant_id", "created_at", "updated_at"]
+
+  get "/api/v1/items/find_all" do
+    if !acceptable_queries.include?(params.keys.first)
+      return ItemSerializer.new(Item.all).to_json
+    else
+      return ItemSerializer.new(Item.find_all(parsed)).to_json
+    end
+  end
+
+  get "/api/v1/items/find" do
+    if !acceptable_queries.include?(params.keys.first)
+      return ItemSerializer.new(Item.all).to_json
+    else
+      return ItemSerializer.new(Item.find_by("#{parsed.keys.first} = ?", parsed.values.first)).to_json
+    end
+  end
+
+  get "/api/v1/items/:id" do
+    ItemSerializer.new(Item.find(params[:id])).to_json
+  end
+
+  get "/api/v1/items" do
+    ItemSerializer.new(Item.all).to_json
+  end
+
+  helpers do
+    def parsed
+      return {"unit_price" => (params[:unit_price].to_r * 100).to_i} if !params[:unit_price].nil?
+      params
+    end
+  end
+end
