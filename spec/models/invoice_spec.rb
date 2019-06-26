62d058d315924d30ac0ee255db7376512afb49c9
davisc0801 <davisc1014@gmail.com>
2019-06-26 16:52:34 -0600

    Correct typo on test

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,6 +1,6 @@
 require "./spec/spec_helper"
 
-RSpec.describe Customer, type: :model do
+RSpec.describe Invoice, type: :model do
   describe 'validations' do
     it {should validate_presence_of :status}
   end
