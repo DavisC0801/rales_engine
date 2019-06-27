be186bf469d7b6f8a486a1d66499f4d69a95f6ec
davisc0801 <davisc1014@gmail.com>
2019-06-27 12:37:58 -0600

    Add favorite customer merchant controller action

--- app/controllers/merchants/merchant_business_logic_controller.rb
+++ app/controllers/merchants/merchant_business_logic_controller.rb
@@ -4,7 +4,7 @@ class MerchantsBusinessLogicController < ApplicationController
     RevenueSerializer.new(Merchant.revenue(params)).to_json
   end
   get "/api/v1/merchants/:id/favorite_customer" do
-    # returns the customer who has conducted the most total number of successful transactions.
+    CustomerSerializer.new(Merchant.favorite_customer(params)).to_json
   end
   get "/api/v1/merchants/:id/customers_with_pending_invoices" do
     # returns a collection of customers which have pending (unpaid) invoices. A pending invoice has no transactions with a result of success. This means all transactions are failed. Postgres has an EXCEPT operator that might be useful. ActiveRecord also has a find_by_sql that might help.
