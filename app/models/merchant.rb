4b29a47ce03919d037bd81d3afb83e4bfa283fb3
davisc0801 <davisc1014@gmail.com>
2019-06-27 11:59:07 -0600

    Add merchant model revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,17 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   validates_presence_of :name
+
+  def self.revenue(params)
+    query = joins(invoices: [:invoice_items, :transactions])
+    .where(id: params[:id])
+    .merge(Transaction.successful)
+    .select("SUM(invoice_items.quantity * invoice_items.unit_price)")
+    .group(:id)
+    if !params[:date].nil?
+      query.where(invoices: {created_at: (params[:date].to_date..params[:date].to_date + 1.day)}).first
+    else
+      query.first
+    end
+  end
 end
