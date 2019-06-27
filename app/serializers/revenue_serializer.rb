f11330ef22d9e5ead63bc0457a39efb92082c4f0
davisc0801 <davisc1014@gmail.com>
2019-06-27 11:59:31 -0600

    Add revenue serializer

---
+++ app/serializers/revenue_serializer.rb
@@ -0,0 +1,8 @@
+class RevenueSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :revenue
+
+  attribute :revenue do |object|
+    (object.sum.to_f / 100).to_s.ljust(2, "0")
+  end
+end
