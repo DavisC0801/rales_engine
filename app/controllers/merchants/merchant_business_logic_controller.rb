500edcb84a8a97562b0fe484c4813e0eb21b6bf9
davisc0801 <davisc1014@gmail.com>
2019-06-28 08:44:59 -0600

    Add total revenue route to merchant controller

--- app/controllers/merchants/merchant_business_logic_controller.rb
+++ app/controllers/merchants/merchant_business_logic_controller.rb
@@ -1,16 +1,16 @@
 class MerchantsBusinessLogicController < ApplicationController
-  # Single Merchant
   get "/api/v1/merchants/:id/revenue" do
     RevenueSerializer.new(Merchant.revenue(params)).to_json
   end
+
   get "/api/v1/merchants/:id/favorite_customer" do
     CustomerSerializer.new(Merchant.favorite_customer(params)).to_json
   end
+  
   get "/api/v1/merchants/:id/customers_with_pending_invoices" do
     CustomerSerializer.new(Merchant.customers_with_pending_invoices(params)).to_json
   end
 
-  # All Merchants
   get "/api/v1/merchants/most_revenue" do
     MerchantSerializer.new(Merchant.most_revenue(params)).to_json
   end
@@ -20,7 +20,6 @@ class MerchantsBusinessLogicController < ApplicationController
   end
 
   get "/api/v1/merchants/revenue" do
-    #returns the total revenue for date x across all merchants
+    TotalRevenueSerializer.new(Merchant.total_revenue(params)).to_json
   end
-  # Assume the dates provided match the format of a standard ActiveRecord timestamp.
 end
