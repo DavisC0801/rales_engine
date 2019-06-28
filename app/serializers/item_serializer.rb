61109d7e3446a8103c6233ed30d0cb0adf0b0bfe
davisc0801 <davisc1014@gmail.com>
2019-06-28 10:35:36 -0600

    Update item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -3,6 +3,6 @@ class ItemSerializer
   attributes :id, :name, :description, :merchant_id
 
   attribute :unit_price do |object|
-    (object.unit_price.to_f / 100).to_s
+    (object.unit_price.to_f.to_r / 100).to_f.to_s
   end
 end
