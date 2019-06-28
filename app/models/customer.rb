a70b60199a023a892955798bc7076c992ca432d2
davisc0801 <davisc1014@gmail.com>
2019-06-28 11:44:35 -0600

    Add favorite merchant function to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,16 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices
 
+  def self.favorite_merchant(params)
+    joins(invoices: [:transactions, :merchant])
+    .merge(Transaction.successful)
+    .where(id: params[:id])
+    .select("merchants.*, COUNT(transactions.id)")
+    .group("merchants.id")
+    .order("count DESC")
+    .first
+  end
+
   def self.find_invoices(params)
     joins(:invoices)
     .where(id: params[:id])
@@ -11,6 +21,7 @@ class Customer < ApplicationRecord
   def self.find_transactions(params)
     joins(invoices: :transactions)
     .where(id: params[:id])
-    .select("transactions.*")
+    .select("merchants.*, COUNT(transactions.id)")
+    .group("merchants.id")
   end
 end
