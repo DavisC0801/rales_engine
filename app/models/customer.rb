c82fd8c9e146eba63f9efb48f4cbb8789c2bd574
davisc0801 <davisc1014@gmail.com>
2019-06-28 11:15:40 -0600

    Add find invoices and find transactions to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,16 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices
+
+  def self.find_invoices(params)
+    joins(:invoices)
+    .where(id: params[:id])
+    .select("invoices.*")
+  end
+
+  def self.find_transactions(params)
+    joins(invoices: :transactions)
+    .where(id: params[:id])
+    .select("transactions.*")
+  end
 end
