81dd097214770244a3d9212a06e393ca8e13b80a
davisc0801 <davisc1014@gmail.com>
2019-06-29 16:07:56 -0600

    Add default scope to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
   validates_presence_of :credit_card_number, :result
-
   scope :successful, -> {where(result: "success")}
+  default_scope {order(id: :asc)}
 end
