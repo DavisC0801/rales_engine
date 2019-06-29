8e4ee50e601120c1a16bc64665d1fa874c113276
davisc0801 <davisc1014@gmail.com>
2019-06-29 16:24:40 -0600

    Add invoice and items fetch methods to invoice items model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,18 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   default_scope {order(id: :asc)}
+
+  def self.invoice(params)
+    joins(:invoice)
+    .where(id: params[:id])
+    .select("invoices.*")
+    .take
+  end
+
+  def self.item(params)
+    joins(:item)
+    .where(id: params[:id])
+    .select("items.*")
+    .take
+  end
 end
