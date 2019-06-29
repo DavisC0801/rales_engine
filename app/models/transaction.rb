3fb59735599cfea23b487e9ded9c82584f05a02c
davisc0801 <davisc1014@gmail.com>
2019-06-29 16:44:16 -0600

    Add find invoice method to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,11 @@ class Transaction < ApplicationRecord
   validates_presence_of :credit_card_number, :result
   scope :successful, -> {where(result: "success")}
   default_scope {order(id: :asc)}
+
+  def self.invoice(params)
+    joins(:invoice)
+    .where(id: params[:id])
+    .select("invoices.*")
+    .take
+  end
 end
