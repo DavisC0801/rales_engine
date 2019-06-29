c8c540c19a2a84d628c2e7901f838c84ecd7b77d
davisc0801 <davisc1014@gmail.com>
2019-06-29 16:36:23 -0600

    Add invoice items and merchant fetch methods to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,19 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   default_scope {order(id: :asc)}
 
+  def self.all_invoice_items(params)
+    joins(:invoice_items)
+    .where(id: params[:id])
+    .select("invoice_items.*")
+  end
+
+  def self.merchant(params)
+    joins(:merchant)
+    .where(id: params[:id])
+    .select("merchants.*")
+    .take
+  end
+
   def self.most_revenue(params)
     joins(invoices: [:transactions, :invoice_items])
     .select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price)")
